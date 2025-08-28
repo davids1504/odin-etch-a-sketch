67e61df6ba0a9e50bae6dd6dc462e771af9e4f05
David <sambunjak.david@gmail.com>
2025-08-28 21:23:47 +0200

    Finish basic etch-a-sketch version

---
+++ scripts.js
@@ -0,0 +1,59 @@
+const gridContainer = document.querySelector(".container");
+
+let squaresInRow = 16;
+let containerWidth = parseInt(window.getComputedStyle(gridContainer).width.replace(/\D/g, ""));
+
+function createSquares() {
+  gridContainer.innerHTML = "";
+  let squaresTotal = squaresInRow * squaresInRow;
+  for (let i = 0; i < squaresTotal; i++) {
+    const square = document.createElement("div");
+    square.style.cssText = `
+        width: ${containerWidth / squaresInRow}px;
+        height: ${containerWidth / squaresInRow}px;
+        border: 1px transparent green;
+    `;
+    switch (i) {
+      case 0:
+        square.style.borderTopLeftRadius = "18px";
+        break;
+      case squaresInRow - 1:
+        square.style.borderTopRightRadius = "18px";
+        break;
+      case squaresTotal - squaresInRow:
+        square.style.borderBottomLeftRadius = "18px";
+        break;
+      case squaresTotal - 1:
+        square.style.borderBottomRightRadius = "18px";
+        break;
+    }
+    gridContainer.appendChild(square);
+  }
+
+  // Set up event listeners for every square
+  let squares = document.querySelectorAll(".container div");
+  squares.forEach((el) => {
+    el.addEventListener("mouseover", () => {
+      el.style.backgroundColor = "greenyellow";
+    });
+
+    el.addEventListener("mouseout", () => {
+      el.style.backgroundColor = "green";
+    });
+  });
+}
+
+createSquares();
+function changeResolution() {
+  const button = document.querySelector(".resolution-change");
+  button.addEventListener("click", () => {
+    squaresInRow = parseInt(prompt("Enter the value: ", 16));
+    if (squaresInRow > 100) {
+      alert("100 is max value! Please enter a valid value.");
+      return;
+    }
+    createSquares();
+  });
+}
+
+changeResolution();
